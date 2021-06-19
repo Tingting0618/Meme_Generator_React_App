8a6f263a41323cf6a87cacdd1711b1c5ecaaffd8
Tingting Duan <duan.tingting@yahoo.com>
2021-06-19 15:03:25 -0500

    completed

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header>
+            <img 
+                src="https://static.wixstatic.com/media/75b2be_798fd3d1f17345db9ffd1a28f1e63b35~mv2.gif" 
+                alt="Problem?"
+            />
+            <p>Meme Generator</p>
+        </header>
+    )
+}
+
+export default Header
