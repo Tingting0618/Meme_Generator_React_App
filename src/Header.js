22cb095932a583489d83f2efac22088abb260e9a
Tingting Duan <duan.tingting@yahoo.com>
2021-06-19 15:04:40 -0500

    completed

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,7 @@ function Header() {
                 src="https://static.wixstatic.com/media/75b2be_798fd3d1f17345db9ffd1a28f1e63b35~mv2.gif" 
                 alt="Problem?"
             />
-            <p>Meme Generator</p>
+            <p>Welcome to Free Meme Generator</p>
         </header>
     )
 }
